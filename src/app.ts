7692639a9c519f6cd21560dc4cead763e7dacd1e
Vaibhvee Prakash <vaibhveeprakash25@gmai.com>
2026-09-12 23:53:02 +0530

    Created the first node and edge

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express'
+import useGraph from "./services/graph.ai.services.js"
+import { HumanMessage } from "@langchain/core/messages";
 
 const app = express()
 
-app.get('/', (req,res)=>{
+app.get('/health', (req,res)=>{
     res.status(200).json({status: 'ok'})
 })
+
+app.post ("/use-graph", async (req,res)=>{
+    await useGraph("What is the capital of France")
+    
+})
+
+
 export default app
